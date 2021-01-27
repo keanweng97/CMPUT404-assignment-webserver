4e59fe568541d33b0d72bb4457de6996d4a3df46
Kean Weng Yap <yapkeanweng1223@gmail.com>
2021-01-27 02:23:41 -0700

    Working server, fail test_get_group

--- server.py
+++ server.py
@@ -30,9 +30,66 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(1024).strip().decode()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # self.request.sendall(bytearray("OK",'utf-8'))
+
+        header = self.data.split('\n')
+        request_line = header[0].split()
+        method = request_line[0]
+        uri = request_line[1]
+        print(uri)
+
+        response = ''
+        if method != "GET":
+            response = self.get_header(405,)
+            self.request.sendall(response.encode())
+        else:
+            if uri[-1] == '/':
+                uri += "index.html"
+            
+            
+            try:
+                file_path = open("www" + uri)
+            except FileNotFoundError:
+                response = self.get_header(404,)
+                response += "404 Error"
+                self.request.sendall(response.encode())
+                return
+            except IsADirectoryError:
+                response = self.get_header(301,)
+                response += f"Location: {uri}/\n\n"
+                self.request.sendall(response.encode())
+                return
+            
+            content = file_path.read()
+            file_path.close()
+
+            if uri[-4:] == '.css':
+                response = self.get_header(200, "text/css")
+            if (uri[-5:] == '.html' and response == ''):
+                response = self.get_header(200, "text/html")
+
+            response += content
+            self.request.sendall(response.encode())
+
+    def get_header(self, status_code, mime_type = "text/html"):
+        header = "HTTP/1.1 "
+        if status_code == 200:
+            header += "200 OK\n"
+        elif status_code == 301:
+            header += "301 Moved Permanently\n"
+        elif status_code == 404:
+            header += "404 Not Found\n"
+        elif status_code == 405:
+            header += "405 Method Not Allowed\n"
+        
+        header += "Server: Simple Python Server\n"
+        if status_code != 301:
+            header += f"Content-Type: {mime_type}\n\n"
+
+        return header 
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
